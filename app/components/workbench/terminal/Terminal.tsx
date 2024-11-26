d17f97ebe705468280f3a022dc290263728379ca
Rob Koch <kochrt@gmail.com>
2024-11-26 13:26:31 -0800

    terminal theme

--- app/components/workbench/terminal/Terminal.tsx
+++ app/components/workbench/terminal/Terminal.tsx
@@ -68,6 +68,9 @@ export const Terminal = memo(
 
       // we render a transparent cursor in case the terminal is readonly
       terminal.options.theme = getTerminalTheme(readonly ? { cursor: '#00000000' } : {});
+      requestAnimationFrame(() => {
+        terminal.options.theme = getTerminalTheme(readonly ? { cursor: '#00000000' } : {});
+      });
 
       terminal.options.disableStdin = readonly;
     }, [theme, readonly]);
